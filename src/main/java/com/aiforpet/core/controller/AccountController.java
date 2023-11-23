6d89bbdf325b27e33741ba4aa54d5b0c6eb45cc4
euichanhwang <pine6971@gmail.com>
2023-11-23 14:15:06 +0900

    [FEAT] 커스텀 에러 애노테이션 설정

--- src/main/java/com/aiforpet/core/controller/AccountController.java
+++ src/main/java/com/aiforpet/core/controller/AccountController.java
@@ -3,9 +3,9 @@ package com.aiforpet.core.controller;
 import com.aiforpet.core.AccountService;
 import com.aiforpet.core.controller.dto.Response;
 import com.aiforpet.core.controller.dto.SignUpReq;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -19,7 +19,7 @@ public class AccountController {
 
     @PostMapping("/new")
     @ResponseStatus(HttpStatus.CREATED)
-    public Response signUp(@RequestBody @Valid final SignUpReq request) { // 유효성 검사를 실패하면 MethodArgumentNotValidException
+    public Response signUp(@RequestBody @Validated final SignUpReq request) {
         return new Response(accountService.create(request));
     }
 }
---
+++ src/main/java/com/aiforpet/core/custom/Phone.java
@@ -0,0 +1,21 @@
+package com.aiforpet.core.custom;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneValidator.class)
+public @interface Phone {
+
+    String message() default "Invalid Phone";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
